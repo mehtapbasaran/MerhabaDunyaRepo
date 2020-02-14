e54605d89e58e2ad30ce1edf4c1be81ea599da9b
Mehtap Basaran <mehtap_gs_95@hotmail.com>
2020-02-14 20:48:24 +0300

    program main method changes

--- MerhabaDunya/Program.cs
+++ MerhabaDunya/Program.cs
@@ -10,6 +10,8 @@ namespace MerhabaDunya
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
